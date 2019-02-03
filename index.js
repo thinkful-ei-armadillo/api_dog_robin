a57a51cdf24c7256d5db7e9aa4d436fa78fdf3ee
Robin Khiv <robinkhiv@gmail.com>
2019-02-02 16:58:26 -0800

    working dog api app

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
+/* eslint-disable no-console */
+/* global $ */ 
 'use strict';
 
-function getDogImages(count) {
-  fetch(`https://dog.ceo/api/breeds/image/random${count === 1 ? '' : `/${count}` }`)
+function getDogImages(value) {
+  const numberInput = (typeof value === 'number') ? `/${value}`: ''; 
+  const stringBreedInput = (typeof value === 'string') ? `/${value}/images` : 's/image';
+ 
+  fetch(`https://dog.ceo/api/breed${stringBreedInput}/random${numberInput}`)
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+    // eslint-disable-next-line no-unused-vars
+    .catch(error => alert('Breed Not Found'));
 }
 
 function displayResults(responseJson) {
-  console.log(responseJson);
   $('h2').html(generatePictures(responseJson));
   $('.results').removeClass('hidden');
 }
 
 function generatePictures(responseJson){
-    let images = "";
-    responseJson.message.forEach(imgLink => images += 
-    `<img src="${imgLink}" class="results-img">\n` );
-    return images;
+  let images = '';
+  if (typeof responseJson.message !== 'string') {
+    responseJson.message.forEach(imgLink => { 
+      images +=  `<img src="${imgLink}" class="results-img">\n`;});
+  } else {
+    images = `<img src="${responseJson.message}" class="results-img">`;
+  } return images;
+}
+
+function watchBreed(){
+  $('.js-breed-form').on('submit', function(event){
+    event.preventDefault();
+    const breedType = $('#dog-breed').val();
+    getDogImages(breedType);
+  });
 }
 
 function watchForm() {
-  $('form').on('submit', event => {
+  $('.js-dog-pic-form').on('submit', event => {
     event.preventDefault();
     const picNum = $('.js-img-number').val();
-    getDogImages(picNum);
+    getDogImages(parseInt(picNum, 10));
   });
 }
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
+  watchBreed();
 });
